70a8a35f31d441b786ab85b1a2477e31ba108937
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-23 15:22:48 +0100

    Creates respond to #top_up RSpec test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'responds to the method #balance' do
     expect(subject).to respond_to(:balance)
   end
+
+  describe '#top_up' do
+    it 'responds to the method #top_up with 1 argument' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+  end
 end
