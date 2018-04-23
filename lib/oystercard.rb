d11e71280b43c3766fe9b8e0b6705c410ee69572
Dione Estabillo <dione.estabillo@gmail.com>
2018-04-23 16:18:19 +0100

    added functionality for card to store a limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,14 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(value)
+    raise "Maximum value exceeded" if @balance + value > LIMIT
+    @balance += value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,5 +9,20 @@ describe Oystercard do
     it 'responds to the method #top_up with 1 argument' do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
+
+    it 'adds money to the balance of the card' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+
+    it 'raises an error during top-up when the balance exceeds 90' do
+      expect { subject.top_up(100)
+      }.to raise_error "Maximum value exceeded"
+    end
+
+    it 'expects balance to equal limit constant' do
+      subject.top_up(90)
+      expect(subject.balance).to eq Oystercard::LIMIT
+    end
   end
 end
